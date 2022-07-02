d67b41ccfdd198e0fc55536841cc3a6b2fa163b3
Priyo Pujonggo <prio.pujonggo@gmail.com>
2022-07-02 15:48:07 +0700

    add package

---
+++ crush.py
@@ -0,0 +1,11 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+theta = np.linspace(0,2 * np.pi, 100)
+x = 16 * (np.sin(theta) ** 3)
+y = 13 * np.cos(theta) - 5 * np.cos(2*theta) - 2 * \
+        np.cos(3*theta) - np.cos(4*theta)
+
+plt.plot(x,y)
+plt.title('My Desire Shape')
+plt.show()
